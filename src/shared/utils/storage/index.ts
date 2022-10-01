0a57b941aa1282e48fefc152cd21774e6e7a566a
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-10-01 01:57:39 -0500

    chore: add prettier config

--- src/shared/utils/storage/index.ts
+++ src/shared/utils/storage/index.ts
@@ -1,49 +1,47 @@
 export const convertJson = (text: string) => {
   try {
-    JSON.parse(text);
+    JSON.parse(text)
   } catch (e) {
-    return false;
+    return false
   }
-  return true;
-};
+  return true
+}
 
 const storageHandler = () => {
   const setItemTransfrom = <T>(value: T) => {
-    return JSON.stringify(value);
-  };
+    return JSON.stringify(value)
+  }
 
   const getItem = <T>(key: string): T | null => {
     try {
-      const rawData = localStorage.getItem(key);
-      if (!rawData) return null;
+      const rawData = localStorage.getItem(key)
+      if (!rawData) return null
 
-      return convertJson(rawData)
-        ? (JSON.parse(rawData) as T)
-        : (rawData as unknown as T);
+      return convertJson(rawData) ? (JSON.parse(rawData) as T) : (rawData as unknown as T)
     } catch (err) {
-      return err as unknown as T;
+      return err as unknown as T
     }
-  };
+  }
 
   const setItem = <T>(key: string, value: T) => {
-    return localStorage.setItem(key, setItemTransfrom<typeof value>(value));
-  };
+    return localStorage.setItem(key, setItemTransfrom<typeof value>(value))
+  }
 
   const removeItem = (key: string) => {
-    localStorage.removeItem(key);
-  };
+    localStorage.removeItem(key)
+  }
 
   const clearItems = () => {
-    localStorage.clear();
-  };
+    localStorage.clear()
+  }
 
   return {
     get: getItem,
     set: setItem,
     remove: removeItem,
     clear: clearItems,
-  };
-};
+  }
+}
 
-const storage = storageHandler();
-export default storage;
+const storage = storageHandler()
+export default storage
