cf41039b90316f6690d5865b35b515666cdd0617
gerardotamo <gerardotamo@ravn.co>
2022-10-08 16:36:48 -0500

    feat: add components for styled

--- src/ui/Template/Template.tsx
+++ src/ui/Template/Template.tsx
@@ -1,3 +1,4 @@
+import { Dropdown, Input, Menu, MenuProps, Select, Space } from 'antd';
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useEffect, useState } from 'react'
 
@@ -5,32 +6,33 @@ import { API } from '../../shared/utils/constant/api'
 import Box from '@mui/material/Box'
 import { Button } from '../Button'
 import CardTemplate from './CardTemplate';
+import { DownOutlined } from '@ant-design/icons';
 import Drawer from '@mui/material/Drawer'
 import HeaderTemplate from './HeaderTemplate'
 import axios from 'axios'
 import styled from 'styled-components'
 
-enum LocationEnum {
-    PE = "PE",
-    USD = "USA",
-    SV = "SV"
-  }
 export interface IFormInput {
-    location: LocationEnum | undefined;
+    location: string;
     template: string | undefined;
     description: string | undefined;
   }
 
+const { TextArea : TextAreaStyled } = Input;
+const { Option } = Select;
+
 export const TemplateForm = () => {
   const [state, setState] = useState({ right: false })
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const { register, handleSubmit, setValue } = useForm<IFormInput>();
   const [template, setTemplate] = useState([]);
+  const [variables, setVariables] = useState<{label:string, key: string}[]>([]);
   
+  const [textTemplate, setTextTemplate] = useState('');
+
     useEffect(()=> {
         const getTemplates = async () => {
             try {
-                const {data} = await axios.get(`${API}/template`)
-                console.log(data)
+                const {data} = await axios.get(`${API}/template`);
                 setTemplate(data)
             } catch (error) {
                 console.log(error)
@@ -39,6 +41,19 @@ export const TemplateForm = () => {
         getTemplates();
     },[])
 
+    useEffect(()=> {
+      const getTemplatesVariables = async () => {
+          try {
+              const {data} = await axios.get(`${API}/template/variables`);
+              console.log(createMenu(data));
+              setVariables(createMenu(data))
+          } catch (error) {
+              console.log(error)
+          }
+      }
+      getTemplatesVariables();
+  },[])
+
   const toggleDrawer = (anchor: 'right', open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
     if (
       event.type === 'keydown' &&
@@ -46,13 +61,12 @@ export const TemplateForm = () => {
     ) {
       return
     }
-
     setState({ ...state, [anchor]: open })
   }
 
   const onSubmit: SubmitHandler<IFormInput> = async data => {
     console.log(data)
-    try {
+    /*try {
         const resp = await axios.post(`${API}/template/`, {
             template: data.template,
             location: data.location,
@@ -64,12 +78,17 @@ export const TemplateForm = () => {
         toggleDrawer('right', false);
     } catch (error) {
         console.log(error)
-    }
+    }*/
+  };
+
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+    setValue('location', value);
   };
 
   const list = (anchor: 'right') => (
     <Box
-      sx={{ width: 340, margin: 2 }}
+      sx={{ width: 380, margin: 2 }}
       role="presentation"
     >
       <TitleDrawer>New Template</TitleDrawer>
@@ -80,18 +99,31 @@ export const TemplateForm = () => {
         accurate info to each of your employees on the day you choose. They only will have to copy and paste the
         information.
       </InfoDrawer>
-      <br />
+      <div style={{zIndex: 2}}>
+      <Dropdown overlay={menu}>
+        <Button style={{width:'100%', fontFamily:'SF Pro Tex'}} type={'dashed'}>
+          <Space>
+            Avalaible Variables
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
+      </div>
+      
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <InfoDrawer>Location</InfoDrawer>
-        <Select {...register("location")}>
-            <Option value={'PE'}>PE</Option>
-            <Option value={'USA'}>USA</Option>
-            <Option value={'SV'}>SV</Option>
-        </Select>
         <InfoDrawer>Template</InfoDrawer>
-        <Input {...register("template")} type={'text'} />
+        <TextArea {...register("template")} onChange={(e)=>{
+          setTextTemplate(e.target.value);
+          setValue('template',e.target.value);
+        }} value={textTemplate} />
+        <InfoDrawer>Select Location</InfoDrawer>
+        <Select {...register("location", {value : "PE" })} size='small' defaultValue="PE" style={{width: "100%"}} onChange={handleChange}>
+            <Option value='PE'>PE</Option>
+            <Option value='USA'>USA</Option>
+            <Option value='SV'>SV</Option>
+        </Select>
         <InfoDrawer>Description</InfoDrawer>
-        <TextArea {...register("description")}>Enter text here....</TextArea>
+        <TextArea {...register("description")} onChange={(e)=> setValue('description',e.target.value) }/>
         <Section>
             <CancelButton onClick={toggleDrawer(anchor, false)}>Cancel</CancelButton>
             <SaveButton onClick={handleSubmit(onSubmit)}>Save</SaveButton>
@@ -99,6 +131,30 @@ export const TemplateForm = () => {
       </Form>
     </Box>
   )
+
+  const handleMenuClick: MenuProps['onClick'] = e => {
+    console.log('click', e.key);
+    setTextTemplate(`${textTemplate} ${e.key}`)
+  };
+
+  const menu = (
+    <Menu
+      onClick={handleMenuClick}
+      items={variables}
+    />
+  );
+  
+    const createMenu = (data: any) => {
+      var keys = Object.keys(data);
+      const items = []
+      for (const k of keys) {
+        items.push({
+          label: k,
+          key: data[k]
+        });
+      }
+      return items;
+    }
 
   return (
     <Container>
@@ -108,12 +164,13 @@ export const TemplateForm = () => {
         accurate info to each of your employees on the day you choose. They only will have to copy and paste the
         information.{' '}
       </TextInfo>
+      
       <Section>
         <>Template</>
         <CreateTemplateButton onClick={toggleDrawer('right', true)}>Create new Template</CreateTemplateButton>
       </Section>
       <HeaderTemplate />
-      <Drawer anchor={'right'} open={state['right']} onClose={toggleDrawer('right', false)}>
+      <Drawer anchor={'right'} open={state['right']} onClose={toggleDrawer('right', false)} style={{position:'relative', zIndex:1}}>
         {list('right')}
       </Drawer>
 
@@ -218,27 +275,31 @@ const Divider = styled('div')`
 `
 
 const InfoDrawer = styled('p')`
+  font-family: 'Raleway';
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
   line-height: 22px;
+  margin-top: 8px;
   /* identical to box height, or 157% */
 
   color: #000000;
 `
 
-const Select = styled('select')`
+/*const Input = styled('input')`
   width: 100%;
-`
+`*/
 
-const Option = styled('option')``
+const TextArea = styled(TextAreaStyled)`
+  //width: 100%;
+  font-family: 'DM Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 20px;
+  /* identical to box height, or 154% */
 
-const Input = styled('input')`
-  width: 100%;
-`
-
-const TextArea = styled('textarea')`
-  width: 100%;
+  letter-spacing: 0.25px;
 `
 
 const CancelButton = styled(Button)`
